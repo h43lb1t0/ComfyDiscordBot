2cd6f65e589ff6d025192b8a33bc722f382117f7
Tom Haelbich <haelbito@outlook.com>
2023-08-30 20:41:02 +0200

    added queueing system

--- discordBot.py
+++ discordBot.py
@@ -2,9 +2,10 @@ import asyncio
 import os
 import sys
 from git import Repo
-from pprint import pprint
 import interactions
 from logger import Logger
+
+
 
 logger = Logger()
 
@@ -15,6 +16,10 @@ if not os.path.exists(os.path.join(destination_dir, '.git')):
     Repo.clone_from(repo_url, destination_dir)
 else:
     print(f"The repository is already cloned at {destination_dir}.")
+    print("Pulling latest changes...")
+    repo = Repo(destination_dir)
+    origin = repo.remotes.origin
+    origin.pull()
 
 sys.path.append(destination_dir)
 
@@ -28,7 +33,7 @@ choices_ads = []
 
 for style in unique_style_names:
     if style.startswith("sai"):
-        choices_sai.append(interactions.SlashCommandChoice(name=style, value=style))
+        choices_sai.append(interactions.SlashCommandChoice(name=style.replace("sai-",""), value=style))
     else:
         choices_ads.append(interactions.SlashCommandChoice(name=style, value=style))
 
@@ -41,20 +46,59 @@ from core import ComfyApi
 
 api = ComfyApi()
     
-bot = interactions.Client(token=discord_bot_token)
+bot = interactions.Client(token=discord_bot_token,
+                          send_not_ready_messages=True,
+                          auto_defer=True)
 
 logger.log_info("I'm ready!")      
 
 demensions = [
     interactions.SlashCommandChoice(name="1024x1024", value="1024x1024"),
-    interactions.SlashCommandChoice(name="896x1152", value="768x1024"),
-    interactions.SlashCommandChoice(name="1152x896", value="768x1024"),
+    interactions.SlashCommandChoice(name="896x1152", value="896x1152"),
+    interactions.SlashCommandChoice(name="1152x896", value="1152x896"),
     interactions.SlashCommandChoice(name="960x1024", value="960x1024"),
     interactions.SlashCommandChoice(name="1024x960", value="1024x960"),
     interactions.SlashCommandChoice(name="768x1024", value="768x1024")    
     ] 
+batch_sizes = [
+    interactions.SlashCommandChoice(name="1", value=1),
+    interactions.SlashCommandChoice(name="2", value=2),
+    interactions.SlashCommandChoice(name="3", value=4),
+    interactions.SlashCommandChoice(name="4", value=4) 
+    ] 
 
 
+
+from asyncio import Queue
+
+task_queue = Queue()
+
+async def worker():
+    while True:
+        if not task_queue.empty():
+            text_g, supporting_prompt, dimensions, batch_size, nsfw, thread_name, thread, autor_mention = await task_queue.get()
+
+            images = await api.generate_images(text_g, supporting_prompt, dimensions, batch_size, nsfw)
+
+            files = []
+            if images:
+                for node_id in images:
+                    for image_data in images[node_id]:
+                        import io
+                        files.append(interactions.File(file=io.BytesIO(await image_data), content_type="image", file_name="img.png"))
+
+                foo = await thread.send(file=files)
+                """ url = foo.attachments[0].url
+                print(url) """
+
+                await thread.send(f"{autor_mention} your images are ready!")
+                print("Files sent.")
+
+                var = interactions.EmbedAttachment(files[0])
+                foo = interactions.Embed(title="Your images are ready!", description=f"{autor_mention} your images are ready!", color=0x00ff00)
+
+                await thread.send(embed=foo)
+        await asyncio.sleep(1)
 
 
 @interactions.slash_command(
@@ -68,12 +112,6 @@ demensions = [
     opt_type=interactions.OptionType.STRING
 )
 @interactions.slash_option(
-    name="supporting_prompt",
-    description="prompt to support the postive prompt",
-    required=False,
-    opt_type=interactions.OptionType.STRING
-)
-@interactions.slash_option(
     name="style",
     description="select a style for your images",
     required=True,
@@ -81,8 +119,14 @@ demensions = [
     choices=choices_sai  
 )
 @interactions.slash_option(
+    name="supporting_prompt",
+    description="prompt to support the postive prompt",
+    required=False,
+    opt_type=interactions.OptionType.STRING
+)
+@interactions.slash_option(
     name="dimensions",
-    description="demensions for the image",
+    description="width, height",
     required=False,
     opt_type=interactions.OptionType.STRING,
     choices=demensions
@@ -94,23 +138,98 @@ demensions = [
     opt_type=interactions.OptionType.INTEGER,
     min_value=1,
     max_value=4,
-    choices=[1, 2, 3, 4]
+    choices=batch_sizes
 )
+ # Global counter to keep track of API calls
+async def create_command(ctx: interactions.SlashContext, prompt : str, style : str, supporting_prompt: str = "", dimensions : str = "1024x1024", batch_size : int = 4, nsfw : bool = False):
 
-async def my_first_command(ctx: interactions.SlashContext, prompt : str, supporting_prompt: str, style : str, dimensions : str = "1024x1024", batch_size : int = 4):
+    print("create_command")
+    await ctx.defer()
+
     text_g, neg_prompt = read_sdxl_templates_replace_and_combine(read_json_file(os.path.join(destination_dir, "sdxl_styles_sai.json")), style, prompt, "")
     msg = await ctx.send(text_g)
-    thread = await msg.create_thread(f"Pictures {ctx.author}")
+
+                                 
+                                 
+
+    thread_name = f"{ctx.author}"
+    thread = await msg.create_thread(thread_name)
+
+    autor_mention = ctx.author.mention
+
+    await thread.send(f"You are at position {task_queue.qsize()} in the queue.\nOne queue position is about 30 seconds up to 2 minutes.")
+
+
+    await task_queue.put((text_g, supporting_prompt, dimensions, batch_size, nsfw, thread_name, thread, autor_mention))
+
+
+    """ images, thread_name = await api.generate_images(text_g, supporting_prompt, dimensions, batch_size, nsfw, thread_name)
+
+    print(f"images for thread {thread_name}")
+
+
     files = []
-    try:
-        images = api.generate_images(text_g, supporting_prompt, dimensions, batch_size)
-    except Exception as e:
-        await ctx.send(f"I'm sorry, but i had trouble connecting to the ComfyUi API\n{e}")
-        return
-    for node_id in images:
-        for image_data in images[node_id]:
-            import io
-            files.append(interactions.File(file=io.BytesIO(image_data), content_type="image", file_name="img.png"))
-    #await ctx.send()
-    await thread.send(file=files)
+    if images:
+        for node_id in images:
+            for image_data in images[node_id]:
+                import io
+                files.append(interactions.File(file=io.BytesIO(await image_data), content_type="image", file_name="img.png"))
+        await thread.send(file=files)
+        print("Files sent.") """
+
+
+
+from interactions import listen
+from interactions.api.events import Startup
+@listen(Startup)
+async def onStartup():
+    asyncio.create_task(worker())
+
+
+@interactions.slash_command(
+    name="create_nsfw",
+    description="create NSFW images with SDXL",
+    nsfw=True
+)
+@interactions.slash_option(
+    name="prompt",
+    description="postive prompt",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+@interactions.slash_option(
+    name="style",
+    description="select a style for your images",
+    required=True,
+    opt_type=interactions.OptionType.STRING,
+    choices=choices_sai  
+)
+@interactions.slash_option(
+    name="supporting_prompt",
+    description="prompt to support the postive prompt",
+    required=False,
+    opt_type=interactions.OptionType.STRING
+)
+@interactions.slash_option(
+    name="dimensions",
+    description="width, height",
+    required=False,
+    opt_type=interactions.OptionType.STRING,
+    choices=demensions
+)
+@interactions.slash_option(
+    name="batch_size",
+    description="amount of images to generate",
+    required=False,
+    opt_type=interactions.OptionType.INTEGER,
+    min_value=1,
+    max_value=4,
+    choices=batch_sizes
+)
+async def nsfw_command(ctx: interactions.SlashContext, prompt : str, style : str, supporting_prompt: str = "", dimensions : str = "1024x1024", batch_size : int = 4):
+    await create_command(ctx, prompt, style, supporting_prompt, dimensions, batch_size, nsfw=True)
+
+
+
 bot.start()
+
