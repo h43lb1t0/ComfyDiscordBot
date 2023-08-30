764c7019f4bbc5fc8d27ebd3ca608fc7b422862a
Tom Haelbich <haelbito@outlook.com>
2023-08-30 20:40:20 +0200

    switched to aiohttp

--- core.py
+++ core.py
@@ -1,56 +1,62 @@
-from datetime import datetime
-import os
 import random
 import sys
-import websocket
+import aiohttp
+import urllib.parse
 import uuid
 import json
-import urllib.request
-import urllib.parse
-import time
+import websockets
 import yaml
 from logger import Logger
+
 
 class ComfyApi():
     
     def read_yml(self, attr: str, value: str):
         with open(self.config_file, 'r') as file:
-            config = yaml.safe_load(file)
+            config = yaml.safe_load(file.read())
         
         info = config.get(attr, {}).get(value)
         if info:
             return info
         else:
             raise ValueError(f"{value} not found in the configuration file")
-    
-    
+
     def __init__(self):
         self.config_file = "config.yml"
         self.server_address = self.read_yml("server", "address")
         self.client_id = str(uuid.uuid4())
-        #self.logger = Logger(self.read_yml("logger", "debug"))
-
-    def queue_prompt(self, prompt):
+        
+    async def queue_prompt(self, prompt):
         p = {"prompt": prompt, "client_id": self.client_id}
         data = json.dumps(p).encode('utf-8')
-        req =  urllib.request.Request("http://{}/prompt".format(self.server_address), data=data)
-        return json.loads(urllib.request.urlopen(req).read())
-
-    def get_image(self, filename, subfolder, folder_type):
+        url = f"http://{self.server_address}/prompt"
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, data=data) as resp:
+                return await resp.json()
+    
+    async def get_image(self, filename, subfolder, folder_type):
         data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
         url_values = urllib.parse.urlencode(data)
-        with urllib.request.urlopen("http://{}/view?{}".format(self.server_address, url_values)) as response:
-            return response.read()
+        url = f"http://{self.server_address}/view?{url_values}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                return await resp.read()
 
-    def get_history(self, prompt_id):
-        with urllib.request.urlopen("http://{}/history/{}".format(self.server_address, prompt_id)) as response:
-            return json.loads(response.read())
+    async def get_history(self, prompt_id):
+        url = f"http://{self.server_address}/history/{prompt_id}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                return await resp.json()
 
-    def get_images(self, ws, prompt):
-        prompt_id = self.queue_prompt(prompt)['prompt_id']
+    async def get_images(self, ws, prompt):
+        result = await self.queue_prompt(prompt)
+        prompt_id = result['prompt_id']
+
         output_images = {}
+        # ... (The rest remains largely the same, except that it will use await for async methods
+
         while True:
-            out = ws.recv()
+            out = await ws.recv()
             if isinstance(out, str):
                 message = json.loads(out)
                 if message['type'] == 'executing':
@@ -60,7 +66,8 @@ class ComfyApi():
             else:
                 continue #previews are binary data
 
-        history = self.get_history(prompt_id)[prompt_id]
+        history = await self.get_history(prompt_id)
+        history = history[prompt_id]
         for o in history['outputs']:
             for node_id in history['outputs']:
                 node_output = history['outputs'][node_id]
@@ -83,10 +90,14 @@ class ComfyApi():
         
     
     
-    async def generate_images(self, prompt_text,supporting_prompt, dimensions: str, batch_size):
+    async def generate_images(self, prompt_text,supporting_prompt, dimensions: str, batch_size, nsfw : bool = False):
 
-        with open("workflow.json", "r") as file:
-            json_content = file.read()
+        if nsfw:
+            with open("workflow_nsfw.json", "r") as file:
+                json_content = file.read()
+        else:
+            with open("workflow.json", "r") as file:
+                json_content = file.read()
 
         prompt = json.loads(json_content)
 
@@ -101,30 +112,24 @@ class ComfyApi():
 
         # prompt
         prompt["75"]["inputs"]["text_g"] = prompt_text # base
-        prompt["75"]["inputs"]["text_l"] = supporting_prompt
+        prompt["75"]["inputs"]["text_l"] += supporting_prompt
         prompt["120"]["inputs"]["text"] = prompt["75"]["inputs"]["text_g"]
 
         # seed
         prompt["22"]["inputs"]["noise_seed"] = random.randint(0, sys.maxsize)
+        #prompt["22"]["inputs"]["noise_seed"] = 1 # for debugging
 
         # batch size
         prompt["5"]["inputs"]["batch_size"] = batch_size
 
 
+        async with websockets.connect(f"ws://{self.server_address}/ws?clientId={self.client_id}") as ws:
+            files = await self.get_images(ws, prompt)
+            return files
+        
+        
 
-        ws = websocket.WebSocket()
-        ws.connect("ws://{}/ws?clientId={}".format(self.server_address, self.client_id))
-        return self.get_images(ws, prompt)
 
 
 
-    """ for node_id in images:
-        date_string = datetime.now().strftime('%Y-%m-%d')
-        if not os.path.isdir(date_string):
-            os.mkdir(date_string)
-        for image_data in images[node_id]:
-            from PIL import Image
-            import io
-            image = Image.open(io.BytesIO(image_data))
-            image.save(f"{date_string}/image-{time.time()}.png") """
 
